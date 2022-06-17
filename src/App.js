c0b6be9b22f9959f3d695a7f0970327c9cb22a0c
Dwightu <yudehui1115@gmail.com>
2022-06-17 00:42:54 -0400

    Add React Router&outline

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
+import Routes from "./Routes";
+
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        2022 uOttawa Challenge
-      </header>
+    <div className="App container py-3">
+      <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
+        <LinkContainer to="/">
+          <Navbar.Brand className="font-weight-bold text-muted px-4">
+            Scratch
+          </Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end px-4">
+          <Nav activeKey={window.location.pathname}>
+            <LinkContainer to="/signup">
+              <Nav.Link>Signup</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Routes />
     </div>
   );
 }
---
+++ src/containers/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+    return (
+        <div className="Home">
+            <div className="lander">
+                <h1>Sign in options page</h1>
+                <p className="text-muted">The sign in flow you will implement has 3 pages:</p>
+            </div>
+        </div>
+    );
+}
