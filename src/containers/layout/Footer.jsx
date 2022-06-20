7e5739d0703b7379b2a8985187929e4c883e1e76
Dwightu <yudehui1115@gmail.com>
2022-06-19 22:14:19 -0400

    Provide Icon links

--- src/containers/layout/Footer.jsx
+++ src/containers/layout/Footer.jsx
@@ -10,26 +10,26 @@ export default function App() {
         <MDBFooter className='footer bg-light text-center text-black'>
             <MDBContainer className='p-4 pb-0'>
                 <section className='mb-4'>
-                    <a className='btn btn-outline-dark btn-floating m-1' href='#!' role='button'>
+                    <a className='btn btn-outline-dark btn-floating m-1' href='https://www.facebook.com/uottawa' role='button'>
                         <MDBIcon fab icon='facebook-f' />
                     </a>
 
-                    <a className='btn btn-outline-dark btn-floating m-1' href='#!' role='button'>
+                    <a className='btn btn-outline-dark btn-floating m-1' href='https://twitter.com/uOttawa' role='button'>
                         <MDBIcon fab icon='twitter' />
                     </a>
 
-                    <a className='btn btn-outline-dark btn-floating m-1' href='#!' role='button'>
+                    <a className='btn btn-outline-dark btn-floating m-1' href='https://www.google.com/search?q=uottawa&sxsrf=ALiCzsbV7SbjMaGEqdhTdryxKJUtn_MtdQ%3A1655691097011&ei=WdevYu8d0eW0BoTsiogL&ved=0ahUKEwivrsf9-br4AhXRMs0KHQS2ArEQ4dUDCA4&uact=5&oq=uottawa&gs_lcp=Cgdnd3Mtd2l6EAMyBAgjECcyBAgjECcyBAgjECcyCgguELEDENQCEEMyBwgAELEDEEMyBwgAELEDEEMyCgguEMcBEK8BEEMyCggAEIAEEIcCEBQyBAgAEEMyCggAEIAEEIcCEBQ6BwgAEEcQsANKBAhBGABKBAhGGABQ1wVY1wVgjwtoAnAAeACAAWeIAWeSAQMwLjGYAQCgAQHIAQjAAQE&sclient=gws-wiz' role='button'>
                         <MDBIcon fab icon='google' />
                     </a>
-                    <a className='btn btn-outline-dark btn-floating m-1' href='#!' role='button'>
+                    <a className='btn btn-outline-dark btn-floating m-1' href='https://www.instagram.com/uottawa/?hl=en' role='button'>
                         <MDBIcon fab icon='instagram' />
                     </a>
 
-                    <a className='btn btn-outline-dark btn-floating m-1' href='#!' role='button'>
+                    <a className='btn btn-outline-dark btn-floating m-1' href='https://www.linkedin.com/school/uottawa/mycompany/verification/' role='button'>
                         <MDBIcon fab icon='linkedin-in' />
                     </a>
 
-                    <a className='btn btn-outline-dark btn-floating m-1' href='#!' role='button'>
+                    <a className='btn btn-outline-dark btn-floating m-1' href='https://github.com/Dwightu' role='button'>
                         <MDBIcon fab icon='github' />
                     </a>
                 </section>
